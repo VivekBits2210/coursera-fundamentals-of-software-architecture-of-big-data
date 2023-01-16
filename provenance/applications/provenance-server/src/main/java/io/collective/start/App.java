d3e81491ded2d10e5ed4db8fa1fc4125b9a4beac
VivekBits2210 <viveknayak2210@gmail.com>
2023-01-15 22:34:08 -0500

    Provenance initial commit

---
+++ provenance/applications/provenance-server/src/main/java/io/collective/start/App.java
@@ -0,0 +1,52 @@
+package io.collective.start;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.collective.articles.ArticleDataGateway;
+import io.collective.articles.ArticleRecord;
+import io.collective.articles.ArticlesController;
+import io.collective.restsupport.BasicApp;
+import io.collective.restsupport.NoopController;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.TimeZone;
+
+
+public class App extends BasicApp {
+    private ArticleDataGateway dataGateway;
+
+    @Override
+    public void start() {
+        super.start();
+
+        { // todo - start the endpoint worker
+
+        }
+    }
+
+    public App(int port) {
+        super(port);
+    }
+
+    @NotNull
+    @Override
+    protected HandlerList handlerList() {
+        dataGateway = new ArticleDataGateway(List.of(
+                new ArticleRecord(10101, "Programming Languages InfoQ Trends Report - October 2019 4", true),
+                new ArticleRecord(10106, "Ryan Kitchens on Learning from Incidents at Netflix, the Role of SRE, and Sociotechnical Systems", true)
+        ));
+
+        HandlerList list = new HandlerList();
+        list.addHandler(new ArticlesController(new ObjectMapper(), dataGateway));
+        list.addHandler(new NoopController());
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        String port = System.getenv("PORT") != null ? System.getenv("PORT") : "8881";
+        App app = new App(Integer.parseInt(port));
+        app.start();
+    }
+}
